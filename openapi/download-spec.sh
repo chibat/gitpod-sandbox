094693c2c147abb761b2c291a624359514c5af4e
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2020-05-17 23:42:47 +0000

    -

---
+++ openapi/download-spec.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/openapitools/openapi-generator/master/modules/openapi-generator/src/test/resources/2_0/petstore.yaml
+
+
+
---
+++ openapi/generate.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+openapi-generator generate -i petstore.yaml -g typescript-fetch -o ./test/
+
+
